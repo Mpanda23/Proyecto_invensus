1408c47a8899c41cdd41bc26c1828b93db7b048f
Mpanda23 <82336217+Mpanda23@users.noreply.github.com>
2024-06-27 06:46:51 -0500

    otro commit

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class AuthService {
-  private apiUrl = 'https://apiinvensus.onrender.com/';
+  private apiUrl = 'https://apiinvensus.onrender.com';
 
   constructor(private http: HttpClient) { }
 
